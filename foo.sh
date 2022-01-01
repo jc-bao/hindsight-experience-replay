a52ad163a5ef0a21f060a8eb172f82f9350a843e
jc-bao <jc-bao@outlook.com>
2022-01-01 08:45:56 +0000

    add foo.sh

---
+++ foo.sh
@@ -0,0 +1,7 @@
+mpirun --allow-run-as-root -np 64 python train.py --n-epochs 200 \
+--env-name PandaTowerBimanual-v2 \
+--curriculum --curriculum-bar 0.9 \ 
+--max-trail-time 10 --use-critic-sum \
+--wandb --project Bimanual --group 2obj \
+--name 2obj_critic_sum_curri --note use_critic_sum_curri \
+--render 
