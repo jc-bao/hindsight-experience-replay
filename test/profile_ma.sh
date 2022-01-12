a409965fb8c9a813ef1d117e046ffcd4daf6f322
jc-bao <jc-bao@outlook.com>
2022-01-12 14:30:10 +0800

    add profile tool

---
+++ test/profile_ma.sh
@@ -0,0 +1,2 @@
+python -m cProfile -o her.prof ../train.py \
+--env-name 'formation_hd_env'
---
+++ test/test_render.sh
@@ -0,0 +1,5 @@
+python ../train.py \
+--n-epochs 2 --n-cycles 2 --n-batches 1 --buffer-size 1000 --batch-size 32 \
+--multi-agent \
+--env-name 'formation_hd_env'
+--render
