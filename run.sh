c76011029009672b6d2bed2b29c9a001e7d2e830
jc-bao <jc-bao@outlook.com>
2022-01-16 12:58:18 +0000

    add change small model

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
-tmux rename-window formation_central
+tmux rename-window hd32_central
 export OMPI_MCA_btl_vader_single_copy_mechanism=none
 mpirun --allow-run-as-root -np 64 python train.py \
 --n-epoch 200 \
 --env-name formation_hd_env --num-agents 32 --dim 2 \
---actor-large \
 --wandb --project Formation --name hd32_central
