be36f818213816a0e9f8093a08bb4b51c5fd1bd2
jc-bao <jc-bao@outlook.com>
2022-01-16 11:19:38 +0800

    add dropout policy

--- scripts/testbench/test_ma.sh
+++ scripts/testbench/test_ma.sh
@@ -1,4 +1,6 @@
 python ../../train.py \
 --n-epochs 2 --n-cycles 2 --n-batches 1 --buffer-size 1000 --batch-size 32 \
---actor-separated \
---env-name 'formation_hd_env'
+--actor-dropout \
+--num-agents 4 --dim 2 \
+--env-name 'formation_hd_env' \
+--resume --model-path '/Users/reedpan/Downloads/model.pt'
