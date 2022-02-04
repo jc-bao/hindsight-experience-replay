69b10629754d942ba9eb801e1961bb5ef2846684
jc-bao <jc-bao@outlook.com>
2022-02-04 15:35:38 +0800

    change attention head number

--- rl_modules/attn_models.py
+++ rl_modules/attn_models.py
@@ -119,11 +119,11 @@ class actor_attn(nn.Module):
         self.goal_size = env_params['goal_size']
         self.obj_obs_size = env_params['obj_obs_size']
         self.robot_obs_size = env_params['robot_obs_size']
-        self.feature_extractor = SelfAttentionExtractor(self.robot_obs_size, self.obj_obs_size+self.goal_size, hidden_size = 64, n_attention_blocks=2, n_heads=1)
+        self.feature_extractor = SelfAttentionExtractor(self.robot_obs_size, self.obj_obs_size+self.goal_size, hidden_size = 64, n_attention_blocks=2, n_heads=2)
         self.mlp = nn.Sequential(
-            *(  [nn.Linear(64, 256), nn.ReLU()] +
-                [nn.Linear(256, 256), nn.ReLU()] +
-                [nn.Linear(256, env_params['action']), nn.Tanh()])
+            *(  [nn.Linear(64, 64), nn.ReLU()] +
+                [nn.Linear(64, 64), nn.ReLU()] +
+                [nn.Linear(64, env_params['action']), nn.Tanh()])
         )
         self._initialize()
 
@@ -165,11 +165,11 @@ class critic_attn(nn.Module):
         self.goal_size = env_params['goal_size']
         self.obj_obs_size = env_params['obj_obs_size']
         self.robot_obs_size = env_params['robot_obs_size']
-        self.feature_extractor = SelfAttentionExtractor(self.robot_obs_size+env_params['action'], self.obj_obs_size+self.goal_size, hidden_size = 64, n_attention_blocks=2, n_heads=1)
+        self.feature_extractor = SelfAttentionExtractor(self.robot_obs_size+env_params['action'], self.obj_obs_size+self.goal_size, hidden_size = 64, n_attention_blocks=2, n_heads=2)
         self.mlp = nn.Sequential(
-            *(  [nn.Linear(64, 256), nn.ReLU()] +
-                [nn.Linear(256, 256), nn.ReLU()] +
-                [nn.Linear(256, 1)])
+            *(  [nn.Linear(64, 64), nn.ReLU()] +
+                [nn.Linear(64, 64), nn.ReLU()] +
+                [nn.Linear(64, 1)])
         )
         self._initialize()
 
--- scripts/testbench/test_panda_num_blocks.sh
+++ scripts/testbench/test_panda_num_blocks.sh
@@ -2,4 +2,4 @@ python ../../train.py \
 --n-epochs 10 --n-cycles 2 --n-batches 1 --buffer-size 1000 --batch-size 32 \
 --env-name 'PandaTowerBimanual-v1' \
 --curriculum --curriculum-init 1 --curriculum-end 6 --curriculum-step 1 --curriculum-bar '-0.1' \
---use-crossattn
+--use-attn
