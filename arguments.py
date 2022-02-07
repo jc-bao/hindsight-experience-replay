9e858241c2af7dac66128a9adaf201c27eecc1ab
jc-bao <jc-bao@outlook.com>
2022-02-07 10:48:52 +0800

    add random delay

--- arguments.py
+++ arguments.py
@@ -86,7 +86,7 @@ def get_args():
     parser.add_argument('--num-agents', type = int, default = 3)
     parser.add_argument('--dim', type = int, default = 3)
     # break symetry
-    parser.add_argument('--extra-reset-steps', type = int, default = 20)
+    parser.add_argument('--extra-reset-steps', action='store_true')
     args = parser.parse_args()
 
     return args
