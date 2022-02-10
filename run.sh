39179eed1085f7ad746803328d5b4ac5fc500620
jc-bao <jc-bao@outlook.com>
2022-02-10 02:31:28 +0000

    fix master slave atten critic network

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-tmux rename-window nobj_attn_handnum
+tmux rename-window hand1_rex_attn4
 export OMPI_MCA_btl_vader_single_copy_mechanism=none
 mpirun --allow-run-as-root -np 64 python train.py \
---n-epochs 200 --env-name PandaTowerBimanualHandNumMix-v1 --use-attn --num-blocks 2 --max-trail-time 10 --trail-mode any --not-relabel-unmoved --random-unmoved --curriculum --curriculum-init 1 --curriculum-end 2 --curriculum-step 0.2 --curriculum-bar 0.7 --wandb --project Bimanual --name nobj_attn_handnum --render --extra-reset-steps --resume
+--n-epochs 100 --env-name PandaTowerBimanualMaxHandover1-v2 --use-attn --num-blocks 4 --max-trail-time 10 --trail-mode any --not-relabel-unmoved --random-unmoved --curriculum --curriculum-init 2 --curriculum-end 4 --curriculum-step 0.5 --curriculum-bar 0.9 --wandb --project Bimanual --name hand1_rex_attn4 --render --resume --model-path '/rl/hindsight-experience-replay/saved_models/hand2_attn4.pt'
