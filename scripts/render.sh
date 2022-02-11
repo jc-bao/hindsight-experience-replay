19d27555a2678fa7393c5b23434503072b3c2f7c
jc-bao <jc-bao@outlook.com>
2022-02-11 22:47:33 +0800

    save best model

--- scripts/render.sh
+++ scripts/render.sh
@@ -1,5 +1,5 @@
 python ../train.py \
 --n-epochs 2 --n-cycles 1 --n-batches 0 --buffer-size 1000 --batch-size 32 \
 --env-name PandaTowerBimanualMaxHandover1-v4 --use-attn --num-blocks 4 \
---gui --resume --model-path '/Users/reedpan/Downloads/1hand_2re.pt' \
+--gui --resume --model-path '/Users/reedpan/Downloads/1hand3re.pt' \
 --curriculum --curriculum-init 4 --curriculum-end 4.5 --curriculum-step 0.1 --curriculum-bar '-0.1' --n-test-rollouts 1
