f86c74eaafe2cfac41d33ca2619b634a4be46453
jc-bao <jc-bao@outlook.com>
2022-02-21 12:09:24 +0800

    consider extra reset steps

--- scripts/render.sh
+++ scripts/render.sh
@@ -1,7 +1,7 @@
 python ../train.py \
 --n-epochs 10 --n-cycles 0 --n-batches 0 --buffer-size 1000 --batch-size 32 --n-test-rollouts 2 \
 --env-name PandaTowerBimanualParallelSlow-v2 \
- --resume --model-path '/Users/reedpan/Downloads/handover2_os08.pt' \
+--resume --model-path '/Users/reedpan/Downloads/handover2_os08.pt' \
 --store-trajectory --store-video \
 --use-attn --num-blocks 4 \
 # --gui \
