649581de1e6c2e72a1a6b05c259318d91dd94e8c
jc-bao <jc-bao@outlook.com>
2022-02-24 21:48:28 +0800

    fix minor bugs

--- scripts/render.sh
+++ scripts/render.sh
@@ -1,9 +1,9 @@
 python ../train.py \
 --n-epochs 10 --n-cycles 0 --n-batches 0 --buffer-size 1000 --batch-size 32 --n-test-rollouts 10 \
---env-name PandaTowerBimanual-v1 \
---resume --model-path '/Users/reedpan/Downloads/nobj_attn3_handnum.pt' \
---env-kwargs '{"os_rate":1, "render":true}' \
---use-attn --num-blocks 3
+--env-name PandaTowerBimanualParallel-v2 \
+--resume --model-path '/Users/reedpan/Downloads/handover2_os08.pt' \
+--env-kwargs '{"os_rate":1, "render":true, "obj_in_hand_rate":0, "subgoal_rate": 1, "reward_type": "final"}' \
+--use-attn --num-blocks 4
 
  # "use_bound": false, "store_trajectory":false, "store_video":false, "obj_in_hand_rate":0}' \
 # --extra-reset-steps
